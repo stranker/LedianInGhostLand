ab08a8fc5ac64025eaa24f3b60857ba65fffa75a
Unknown <dani_mrx93@hotmail.com>
2018-05-04 23:01:15 -0300

    V0.5
    Se agrega casi todo

---
+++ Ledian in Ghostland/Assets/Scripts/ObjectScript/Flowerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flowerator : MonoBehaviour {
+    [SerializeField] GameObject flowerPrefab;
+    [SerializeField] GameObject shootPosition;
+    private GameObject flower;
+    private float fireTime;
+    private int fireRate;
+    private bool canShoot;
+    private int speed;
+    // Use this for initialization
+    void Start () {
+        fireRate = 2;
+        fireTime = fireRate;
+        canShoot = true;
+        speed = 40;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!canShoot)
+        {
+            fireTime -= Time.deltaTime;
+        }
+        if (fireTime <= 0)
+        {
+            canShoot = true;
+            fireTime = fireRate;
+        }
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            canShoot = false;
+            flower = Instantiate(flowerPrefab, shootPosition.transform.position,transform.rotation,transform.parent.parent.parent);
+            flower.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * speed;
+        }
+	}
+}
---
+++ Ledian in Ghostland/Assets/Scripts/ObjectScript/Trap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Trap : MonoBehaviour {
+    private int points;
+	// Use this for initialization
+	void Start () {
+        points = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<FirstPersonController>().TakeDamage(50);
+        }
+    }
+
+    public void OnDestroy()
+    {
+        GameManager.SetPoints(GameManager.GetPoints() + points);
+    }
+}
